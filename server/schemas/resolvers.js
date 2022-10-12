891c3bbbfc94d209efb5a2511a05a025f88ff704
janae.wel <janae.wel@gmail.com>
2022-10-12 14:26:40 -0400

    server bugs all solved!

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,57 +1,57 @@
-const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
-const { signToken } = require('../utils/auth');
+const {AuthenticationError} = require('apollo-server-express');
+const {User, Thought} = require('../models');
+const {signToken} = require('../utils/auth');
 
 const resolvers = {
   Query: {
     // get your own user details
     me: async (parent, args, context) => {
       if (context.user) {
-        const userData = await User.findOne({ _id: context.user._id })
+        const userData = await User.findOne({_id: context.user._id})
           .select('-__v -password')
           .populate('thoughts')
           .populate('friends');
 
         return userData;
-      }
+    }
       throw new AuthenticationError('Not logged in');
-    },
+},
     // get all users
     users: async () => {
       return User.find()
         .select('-__v -password')
         .populate('thoughts')
         .populate('friends');
-    },
+},
     // parent param is not used but is required since 2nd param should be the query params (not the 1st)
     // get a user by id
-    user: async (parent, { username }) => {
-      return User.findOne({ username })
+    user: async (parent, {username}) => {
+      return User.findOne({username})
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
-    },
+},
       // get thought (or a specfic user's thoughts)
-      thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
+      thoughts: async (parent, {username}) => {
+      const params = username ? {username} : {};
+      return Thought.find(params).sort({createdAt: -1});
+},
     // get thoughts by id
-    thought: async (parent, { _id }) => {
-      return Thought.findOne({ _id });
-    }
-  },
+    thought: async (parent, {_id}) => {
+      return Thought.findOne({_id});
+}
+},
 
   Mutation: {
     // Mongoose will create a new user with whatever is passed into args
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
-      return { token, user };
+      return {token, user };
     },
-    login: async (parent, { email, password }) => {
+    login: async (parent, {email, password }) => {
       // checking if user exists
-      const user = await User.findOne({ email });
+      const user = await User.findOne({email });
       if (!user) {
         throw new AuthenticationError('Incorrect credentials');
       }
@@ -61,46 +61,46 @@ const resolvers = {
         throw new AuthenticationError('Incorrect credentials');
       }
       const token = signToken(user);
-      return { token, user };
+      return {token, user };
     },
     addThought: async (parent, args, context) => {
       if (context.user) {
         // spread operator gets everything in args individually and then also adds context.user.username to the object
-        const thought = await Thought.create({ ...args, username: context.user.username });
+        const thought = await Thought.create({...args, username: context.user.username });
         await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $push: { thoughts: thought._id } },
-          { new: true }
+          {_id: context.user._id },
+          {$push: {thoughts: thought._id } },
+          {new: true }
         );
         return thought;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+    addReaction: async (parent, {thoughtId, reactionBody }, context) => {
         // remember: Mongo will take the Thought id and push this reaction into an array in Thought object
         if (context.user) {
         const updatedThought = await Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          { $push: { reactions: { reactionBody, username: context.user.username } } },
-          { new: true, runValidators: true }
+          {_id: thoughtId },
+          {$push: {reactions: {reactionBody, username: context.user.username}}},
+          {new: true, runValidators: true}
         );
         return updatedThought;
-      }
+    }
       throw new AuthenticationError('You need to be logged in!');
     },
-    addFriend: async (parent, { friendId }, context) => {
+    addFriend: async (parent, {friendId}, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
+          {_id: context.user._id},
           // unlike $push, $addToSet prevents duplicate entries
-          { $addToSet: { friends: friendId } },
-          { new: true }
+          {$addToSet: {friends: friendId}},
+          {new: true}
         ).populate('friends');
         return updatedUser;
-      }
-      throw new AuthenticationError('You need to be logged in!');
     }
-  }
+      throw new AuthenticationError('You need to be logged in!');
+}
+}
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,48 +1,52 @@
 // import GraphQL tagged template function (so we can use its advanced template literals)
-const {gql} = require('apollo-server-express');
+const { gql } = require('apollo-server-express');
 // create typeDefs
 const typeDefs = gql`
-type User {
+  type User {
     _id: ID
     username: String
     email: String
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
-}
-type Thought {
+  }
+
+  type Thought {
     _id: ID
     thoughtText: String
     createdAt: String
     username: String
     reactionCount: Int
     reactions: [Reaction]
-}    
-type Reaction {
+  }
+
+  type Reaction {
     _id: ID
     reactionBody: String
     createdAt: String
     username: String
-}
-type Auth {
+  }
+
+  type Auth {
     token: ID!
     user: User
-}
-type Query {
+  }
+
+  type Query {
     me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
-}
-type Mutation {
+  }
+
+  type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
     addThought(thoughtText: String!): Thought
     addReaction(thoughtId: ID!, reactionBody: String!): Thought
     addFriend(friendId: ID!): User
-}
+  }
 `;
 
-// export typeDefs
 module.exports = typeDefs;
