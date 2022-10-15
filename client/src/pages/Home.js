c6829093c91866deabf023a7bd301e615ac50881
janae.wel <janae.wel@gmail.com>
2022-10-15 13:44:34 -0400

    fixed all the file deletion bugs

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,29 +1,26 @@
 import React from 'react';
-// allows us to make reqs to the GraphQL server
-import {useQuery} from '@apollo/client';
-// imports our query from utils
-import { QUERY_THOUGHTS } from '../utils/queries';
-// import component
 import ThoughtList from '../components/ThoughtList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
 
 const Home = () => {
   // use useQuery hook to make query request
   // loading: an async feature provided by Apollo to indicate that the req is not yet complete
   // data: the response with the data requested in the query
-  const {loading, data} = useQuery(QUERY_THOUGHTS);
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
   // this 'optional chaining' syntax, is saying 'if data exists, store it in the thoughts const. else, create an empty arr'
   // the empty arr is a fallback. since the async useQuery() won't get all the data right away, we have a failsafe
   const thoughts = data?.thoughts || [];
-  console.log(thoughts);
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <ThoughtList thoughts={thoughts} title="Some Feed for Thought..." />
-          )}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {loading ? (<div>Loading...</div>) : (
+            <ThoughtList thoughts={thoughts} title="Some Feed for Thought..."/>
+          )}
+        </div>
       </div>
     </main>
   );
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-
+  // this does not immediately execute as a useQuery would
+  // Instead, it makes addUser = ADD_USER. When addUser is called the query will be ran
+  const [addUser, {error}] = useMutation(ADD_USER);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -16,6 +20,18 @@ const Signup = () => {
   // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    // using try/catch because we can then use async await instead of .then() .catch() to handle errors
+    try {
+      // 'data = the executed addUser mutation with data from the formState object
+      // we destructure {data} to get just the response we need and not the whole object that GraphSQL returns
+      const {data} = await addUser({
+        // each property in the formState array
+        variables: {...formState}
+      });
+      console.log(data);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -56,6 +72,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
